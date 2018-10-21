2b7e7d90296bc261d053a2be8e1ca1ffe7dbe730
Henrik Olsson <hnrklssn@gmail.com>
2018-10-21 17:05:13 +0200

    spincontroller

--- src/todo/SpinController.java
+++ src/todo/SpinController.java
@@ -6,7 +6,11 @@ import done.AbstractWashingMachine;
 
 
 public class SpinController extends PeriodicThread {
-	AbstractWashingMachine washingMachine;
+	private AbstractWashingMachine washingMachine;
+    private int mode;
+	private WashingProgram program;
+	private boolean spinLeft;
+	private boolean isFresh;
 
 	public SpinController(AbstractWashingMachine mach, double speed) {
 		super((long) (1000/speed)); // TODO: replace with suitable period
@@ -14,6 +18,32 @@ public class SpinController extends PeriodicThread {
 	}
 
 	public void perform() {
-		// TODO: implement this method
+        SpinEvent event = (SpinEvent) mailbox.tryFetch();
+        //event is null if there is no new task given
+        if (event != null) { //aka if new task given
+            mode = event.getMode();
+            program = (WashingProgram) event.getSource();
+            isFresh = true;
+            //ackAlreadySent=false; TODO: maybe send ACK?
+        }
+
+
+        switch (mode) {
+            case SpinEvent.SPIN_OFF:
+                if(isFresh) washingMachine.setSpin(AbstractWashingMachine.SPIN_OFF);
+                break;
+            case SpinEvent.SPIN_SLOW:
+                if(spinLeft) {
+                    washingMachine.setSpin(AbstractWashingMachine.SPIN_LEFT);
+                } else {
+                    washingMachine.setSpin(AbstractWashingMachine.SPIN_RIGHT);
+                }
+                spinLeft = !spinLeft;
+                break;
+            case SpinEvent.SPIN_FAST:
+                if(isFresh) washingMachine.setSpin(AbstractWashingMachine.SPIN_FAST);
+                break;
+        }
+        isFresh = false;
 	}
 }
