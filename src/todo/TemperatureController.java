8e76aff3a1dc1b0fcc21ae6beb4ee19992e44e48
Fabian Sundholm <fabian.sundholm@gmail.com>
2018-10-21 02:24:36 +0200

    Tror att TemperatureController ska funka nu

--- src/todo/TemperatureController.java
+++ src/todo/TemperatureController.java
@@ -7,13 +7,55 @@ import done.AbstractWashingMachine;
 
 public class TemperatureController extends PeriodicThread {
 	AbstractWashingMachine washingMachine;
+	TemperatureEvent event;
+	double targetTemp;
+	WashingProgram program;
+	boolean ackAlreadySent=false;
+	int mode;
 	public TemperatureController(AbstractWashingMachine mach, double speed) {
 		super((long) (1000/speed)); // TODO: replace with suitable period
 		washingMachine = mach;
+		//targetTemp = washingMachine.getTemperature();
 	}
 
+
 	public void perform() {
-		//här ska man tydligen faktiskt göra sakerna, vilken tur att det definitivt framgick någonstans
+
+		event = (TemperatureEvent) mailbox.tryFetch();
+
+
+		//event is null if there is no new task given
+		if (event != null) { //aka if new task given
+			targetTemp = event.getTemperature();
+			program = (WashingProgram) event.getSource();
+			ackAlreadySent=false;
+			mode=event.getMode();
+		}
+
+
+		switch (mode) {
+			case TemperatureEvent.TEMP_IDLE :
+				if (washingMachine.getTemperature() >= targetTemp) {
+					washingMachine.setHeating(false);
+				} else if (targetTemp - washingMachine.getTemperature() >= 2 && washingMachine.getWaterLevel()>0) {
+					washingMachine.setHeating(true);
+				}
+
+				break;
+
+
+			case TemperatureEvent.TEMP_SET :
+				if (washingMachine.getTemperature() >= targetTemp) {
+					washingMachine.setHeating(false);
+					program.putEvent(new AckEvent(this));
+					ackAlreadySent=true;
+				} else if (targetTemp - washingMachine.getTemperature() >= 2 && washingMachine.getWaterLevel()>0) {
+					washingMachine.setHeating(true);
+					System.out.println("heat on");
+				}
+				break;
+
+		}
 	}
 
 }
